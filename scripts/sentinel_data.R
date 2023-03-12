4f6627debda5e6fdba5538229aa69b7396217008
mmcini <mrcl.mancini@gmail.com>
2023-03-12 00:06:56 -0600

    updated sentinel code to use terra and automate the use of other bands

--- scripts/sentinel_data.R
+++ scripts/sentinel_data.R
@@ -2,7 +2,7 @@ source("scripts/_functions.R")
 
 #query_list <- s2_list(server = c("scihub", "gcloud"), tile = "15SXT", orbit = "126",
 #                     time_interval = c("2018-07-02", "2019-07-01"))
-save_path <- "D:/Arquivos/temp_rasters_stoneville/"
+save_path <- "GIS/sentinel2_data/"
 ## safe_is_online("C:/Users/nello/DOCUME~1/SEN2R~1/lta_orders/lta_20230303_153543.json")
 #s2_download(query_list, outdir = save_path)
 
@@ -45,7 +45,7 @@ clouded_dates <- cloud_masks_table %>%
                  filter(area_percent > 0)
 
 ## Loading rasters
-file_names <- paste0(save_path, list.files(save_path, "B\\d\\d(_10m)?\\.jp2$", recursive = T))
+file_names <- paste0(save_path, list.files(save_path, "B\\d\\d(_[1,2]0m)?\\.jp2$", recursive = T))
 file_bands <- str_extract(file_names, "B\\d{2}")
 file_dates <- str_extract(file_names, "\\d{8}") %>%
               as.Date("%Y%m%d")
@@ -56,57 +56,55 @@ date_range <- c("2018-01-01", "2020-07-01") ## period of raster images to select
 
 used_files <- file_path_table %>%
               filter(between(dates, as.Date(date_range[1]), as.Date(date_range[2]))) %>%
-              filter(!dates %in% clouded_dates$dates, bands == "B12") %>% ## filtering clouds
+              filter(!dates %in% clouded_dates$dates,
+                     bands == "B08" |
+                     bands == "B11" |
+                     bands == "B12") %>% ## filtering clouds
               arrange(dates)
 
 used_files <- used_files %>%
+              distinct(dates, bands, .keep_all = T) %>% 
               filter(dates != as.Date("2018-06-05"))
 
-used_files <- used_files %>%
-              mutate(days_diff = days_difference(used_files$dates))
+reference_raster <- rast(used_files$path[1]) %>%
+                    crop(area_of_interest) %>%
+                    mask(area_of_interest)
 
-list_of_bricks <- list()
+rast(list_of_stacks[[1]], nlyrs = 3)
+
+band_names <- unique(used_files$bands)
+list_of_stacks <- list()
 for (i in unique(as.character(used_files$dates))) {
   paths <- used_files %>%
-           filter(dates == i, bands == "B08") %>%
+           filter(dates == i) %>%
            select(path)
-  raster <- crop(stack(paths[[1]]), area_of_interest)
-  list_of_bricks[[i]] <- mask(raster, area_of_interest)
-  crs(list_of_bricks[[i]]) <- crs
-  len <- length(names(list_of_bricks[[i]])) ## some bands have more than 1 layer, idk why!!
-  if (len > 1) {band_names <- rep("B8", len)} else {band_names <- "B8"} ## this fixes the multilayer problem
-  names(list_of_bricks[[i]]) <- band_names
+  raster <- resample_bands(paths$path, reference_raster, area_of_interest) 
+  list_of_stacks[[i]] <- mask(raster, area_of_interest)
+  crs(list_of_stacks[[i]]) <- crs
+  names(list_of_stacks[[i]]) <- band_names
 }
 
-stack <- stack(list_of_bricks)
-stats <- app(as(stack, "SpatRaster"), var)
-plot(stats)
-writeRaster(stats, "GIS/bare_soil_temporal_bands/bare_soils_b11_var.tif")
+combined_rasters <- rast(list_of_stacks)
+indices <- rep(c(1, 2, 3), nlyr(combined_rasters) / 3) ## indices of groups of layers
 
-stats_table <- tibble(dates = character(), means = numeric(), sds = numeric(), ranges = numeric())
-for (i in names(list_of_bricks)) {
-  raster <- list_of_bricks[[i]]
-  mean <- global(as(raster, "SpatRaster"), "mean", na.rm = T)
-  sd <- global(as(raster, "SpatRaster"), "sd", na.rm = T)
-  range <- global(as(raster, "SpatRaster"), "range", na.rm = T)
-  stats_table <- stats_table %>%
-                 add_row(dates = i, means = mean$mean, sds = sd$sd, ranges = range$range)
-}
-
+stats_table <- list()
+bands <- unique(used_files$bands)
+bands <- rep(bands, nlyr(combined_rasters) / length(bands))
+stats_table <- tibble(dates = used_files$dates, band = bands,
+                      means = global(combined_rasters, "mean", na.rm = T)$mean,
+                      sds = global(combined_rasters, "sd", na.rm = T)$sd) %>%
+               filter(means > 0)
 stats_table$dates <- as.Date(stats_table$dates)
-
-stats_table <- read_csv("tables/nir_test.csv")
-stats_table <- stats_table %>%
-               filter(means != 0)
 
 ggplot(stats_table, aes(x = dates, y = means)) +
        geom_point() + geom_line() + geom_smooth(se = F) +
        geom_label(aes(label = format.Date(dates, format = "%d"))) +
+       facet_wrap(.~band) +
        scale_x_date(date_breaks = "1 month",
                     date_labels = "%b/%y")
 
-## Temporary manual selection of data
-bare_soil_dates <- stats_table
+## Manual selection of data
+bare_soil_dates <- stats_table$dates
 
 dates_range1 <- c("2018-03-22", "2018-05-16")
 
@@ -115,40 +113,42 @@ dates_range2 <- c("2018-11-17", "2019-01-31")
 dates_range3 <- c("2019-03-01", "2019-05-15")
 
 used_bare_soil_files1 <- used_files %>%
-                        filter(dates %in% bare_soil_dates$dates) %>%
-                        filter(between(dates, as.Date(dates_range1[1]), as.Date(dates_range1[2])))
+                         filter(dates %in% bare_soil_dates) %>%
+                         filter(between(dates, as.Date(dates_range1[1]), as.Date(dates_range1[2])))
 used_bare_soil_files2 <- used_files %>%
-                        filter(dates %in% bare_soil_dates$dates) %>%
-                        filter(between(dates, as.Date(dates_range2[1]), as.Date(dates_range2[2])))
+                         filter(dates %in% bare_soil_dates) %>%
+                         filter(between(dates, as.Date(dates_range2[1]), as.Date(dates_range2[2])))
 used_bare_soil_files3 <- used_files %>%
-                        filter(dates %in% bare_soil_dates$dates) %>%
-                        filter(between(dates, as.Date(dates_range3[1]), as.Date(dates_range3[2])))
+                         filter(dates %in% bare_soil_dates) %>%
+                         filter(between(dates, as.Date(dates_range3[1]), as.Date(dates_range3[2])))
 used_bare_soil_files <- rbind(used_bare_soil_files1, used_bare_soil_files2, used_bare_soil_files3)
 
-list_of_bricks <- list()
+band_names <- unique(used_bare_soil_files$bands)
+list_of_stacks <- list()
 for (i in unique(as.character(used_bare_soil_files$dates))) {
   paths <- used_files %>%
-           filter(dates == i, bands == "B12") %>%
+           filter(dates == i) %>%
            select(path)
-  raster <- crop(stack(paths[[1]]), area_of_interest)
-  list_of_bricks[[i]] <- mask(raster, area_of_interest) ##CROP BEFORE DOING THIS!!
-  crs(list_of_bricks[[i]]) <- crs
-  len <- length(names(list_of_bricks[[i]])) ## some bands have more than 1 layer, idk why!!
-  if (len > 1) {band_names <- rep("B12", len)} else {band_names <- "B12"} ## this fixes the multilayer problem
-  names(list_of_bricks[[i]]) <- band_names
+  raster <- resample_bands(paths$path, reference_raster, area_of_interest) 
+  list_of_stacks[[i]] <- mask(raster, area_of_interest)
+  crs(list_of_stacks[[i]]) <- crs
+  names(list_of_stacks[[i]]) <- band_names
 }
 
-bare_soil_rasters <- stack(list_of_bricks)
+combined_rasters <- rast(list_of_stacks)
+indices <- rep(c(1, 2, 3), nlyr(combined_rasters) / 3) ## indices of groups of layers
 
-bare_soil_rasters_mean <- app(as(bare_soil_rasters, "SpatRaster"), mean)
-plot(bare_soil_rasters_mean)
-bare_soil_rasters_var <- app(as(bare_soil_rasters, "SpatRaster"), var)
-plot(bare_soil_rasters_var)
+band_means <- tapp(combined_rasters, indices, "mean")
+names(band_means) <- c("B8", "B11", "B12")
+band_vars <- tapp(combined_rasters, indices, var)
+names(band_vars) <- c("B8", "B11", "B12")
 
-writeRaster(bare_soil_rasters_mean, "GIS/bare_soil_temporal_bands/bare_soils_b12_mean.tif")
-writeRaster(bare_soil_rasters_var, "GIS/bare_soil_temporal_bands/bare_soils_b12_var.tif")
+write_paths_means <- paste0("GIS/bare_soil_temporal_bands/bare_soils_",
+                           unique(used_bare_soil_files$bands),
+                           "_means.tif")
+write_paths_vars <- paste0("GIS/bare_soil_temporal_bands/bare_soils_",
+                          unique(used_bare_soil_files$bands),
+                          "_vars.tif")
 
-bare_soil_rasters_mean <- rast("GIS/bare_soil_temporal_bands/bare_soils_B8_mean.tif")
-
-plot(bare_soil_rasters_mean)
-
+writeRaster(band_means, write_paths_means)
+writeRaster(band_vars, write_paths_vars)
